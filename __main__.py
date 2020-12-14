2c094ce5ef1ce84f03aa140ff865034ba8199059
Endy Iskandar Imam <imam.endy@gmail.com>
2020-12-13 22:28:18 -0500

    Refactor main with argsparse

--- __main__.py
+++ __main__.py
@@ -1,41 +1,48 @@
+from argparse import ArgumentParser
+
 from datetime import datetime
 from dateutil import tz
-import shutil
+
+from scripts.runner import run_module, make_boilerplate
 
 
-def _make_boilerplate(day):
-    shutil.copytree('day00', f'day{day:02}')
-    shutil.move(f'day{day:02}/day00.py', f'day{day:02}/day{day:02}.py')
-    open(f'day{day:02}/input.txt', 'a').close()
-
-
-REQUIRED_METHODS = ('part_one', 'part_two')
-def _import_module(day):
-    module_name = f'day{day:02}.day{day:02}'
-    return __import__(module_name, fromlist=REQUIRED_METHODS)
-
-def _run_module(day):
-    print(f'Day {day:02}')
-    try:
-        module = _import_module(day)
-        print('  Part 1:', module.part_one())
-        print('  Part 2:', module.part_two())
-    except ModuleNotFoundError as e:
-        print(' ', e)
-        print('  Making new directory from boilerplate...', end='')
-        _make_boilerplate(day)
-        print('DONE!')
-    except Exception as e:
-        print(' ', e)
-
-
-def _get_days():
+def get_days():
     start = datetime(2020, 12, 1, tzinfo=tz.gettz('EST'))
     curr  = datetime.now(tz=tz.tzlocal())
     days  = (curr - start).days + 1
     return min(days, 25)
 
 
+parser = ArgumentParser(prog='python .')
+parser.add_argument('action',
+                    action='store',
+                    nargs='?',
+                    choices=['run', 'make', 'tex'],
+                    default='run')
+parser.add_argument('-d', '--day',
+                    action='store',
+                    type=int,
+                    default=None)
+
+def raise_empty_days_error():
+    parser.error(f'argument -d/--day: invalid choice: {args.day} '
+                 '(choose between 1 to 25 inclusive)')
+
+
 if __name__ == "__main__":
-    for i in range(1, _get_days() + 1):
-        _run_module(i)
+    args = parser.parse_args()
+    if args.day is not None and args.day not in range(1, 26):
+        raise_empty_days_error()
+
+    if args.action == 'run':
+        if args.day is None:
+            for i in range(1, get_days() + 1):
+                run_module(i)
+        else:
+            run_module(args.day)
+    elif args.day is None:
+        raise_empty_days_error()
+    elif args.action == 'make':
+        make_boilerplate(args.day)
+    elif args.action == 'tex':
+        pass
---
+++ scripts/runner.py
@@ -0,0 +1,21 @@
+import shutil
+
+
+def make_boilerplate(day):
+    shutil.copytree('day00', f'day{day:02}')
+    shutil.move(f'day{day:02}/day00.py', f'day{day:02}/day{day:02}.py')
+    open(f'day{day:02}/input.txt', 'a').close()
+
+
+def run_module(day):
+    print(f'Day {day:02}')
+    try:
+        module_name = f'day{day:02}.day{day:02}'
+        module = __import__(module_name, fromlist=('part_one', 'part_two'))
+        print('  Part 1:', module.part_one())
+        print('  Part 2:', module.part_two())
+    except ModuleNotFoundError as e:
+        print(' ', e)
+        print(f'  Do you mean `python . make -d {day}`?')
+    except Exception as e:
+        print(' ', e)
